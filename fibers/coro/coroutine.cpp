65262912c94ba096dd674f4d72340a66bdf74816
Georgiy Novoselov <nfarost@gmail.com>
2023-07-11 00:15:07 +0300

    coroutine done

---
+++ fibers/coro/coroutine.cpp
@@ -0,0 +1,69 @@
+#include "coroutine.hpp"
+
+#include <exception>
+#include <twist/ed/local/ptr.hpp>
+
+#include <wheels/core/assert.hpp>
+#include <wheels/core/compiler.hpp>
+
+Coroutine* Coroutine::currentCoroutine = nullptr;
+
+static const size_t kDefaultStackSize = 64'000'000;
+
+Coroutine::Coroutine(Routine routine) 
+  : routine_(std::move(routine))
+  , stack_(sure::Stack::AllocateBytes(kDefaultStackSize))
+  , exception_()
+  , completed_(false)
+{
+  context.Setup(stack_.MutView(), this);
+}
+
+void Coroutine::Resume() 
+{
+  if (completed_)
+  {
+    return;
+  }
+
+  Coroutine* callerCoroutine = std::exchange(currentCoroutine, this);
+
+  outerContext.SwitchTo(context);
+
+  currentCoroutine = callerCoroutine;
+
+  if (exception_)
+  {
+    std::rethrow_exception(exception_);
+  }
+}
+
+void Coroutine::Suspend() 
+{
+  currentCoroutine->SuspendInternal();
+}
+
+bool Coroutine::IsCompleted() const 
+{
+  return completed_;
+}
+
+void Coroutine::Run() noexcept 
+{
+  try
+  {
+    routine_();
+  }
+  catch(...)
+  {
+    exception_ = std::current_exception();
+  }
+
+  completed_ = true;
+  context.ExitTo(outerContext);
+}
+
+void Coroutine::SuspendInternal() 
+{
+  context.SwitchTo(outerContext);
+}
